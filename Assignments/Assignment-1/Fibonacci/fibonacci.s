a43bf9477682692d6b0f26ef1871981baa76808e
Sourabh Kumar <sourabhkumar0308@gmail.com>
2018-10-25 13:06:04 +0530

    assignment

--- Assignments/Assignment-1/Fibonacci/fibonacci.s
+++ Assignments/Assignment-1/Fibonacci/fibonacci.s
@@ -9,7 +9,7 @@ __main  FUNCTION
 loop	CMP r3,#0 ; (counter check (n>0))
 		SUBGT r3,r3,#1 ; (decrement counter by 1)
 		BEQ stop; jump to stop once n=0
-		MOV r8,r1 ; (store r1, it stores and displays the fibonacci numbers)
+		MOV r8,r1 ; (store r1, r8 stores and displays the fibonacci numbers)
 		ADD r4,r1,r2 ; (c = a+b)
 		MOV r1,r2 ; (a = b)
 		MOV r2,r4 ; (b = c)
